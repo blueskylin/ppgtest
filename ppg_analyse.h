55749ca5b82a4f48b695a4bb8102f3912b264e10
blueskylin <47210138+blueskylin@users.noreply.github.com>
2020-12-01 20:18:04 +0800

    Update ppg_analyse.h
    del chinese commit

--- ppg_analyse.h
+++ ppg_analyse.h
@@ -13,13 +13,8 @@ typedef struct _SPO2Parameter
 } SPO2Parameter;
 
 
-/******** 初始化脉率计算模块           ********/
 typedef void (*InitDetectPR)();
 
-/******** 实时脉率计算                 ********
- ******** parameter 采集的血氧参数     ********
- ******** 返回值 无起搏返回0 有返回脉率********
- ******** 失败返回-1                   ********/
 typedef int (*DetectPR)(SPO2Parameter *parameter);
 
 
